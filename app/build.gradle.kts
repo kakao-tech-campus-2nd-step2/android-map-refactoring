f6cb34482e35f96a094f655c911fb02eb29c159e
khyeonm <101093716+khyeonm@users.noreply.github.com>
2024-07-16 00:13:27 +0900

    부산대 Android_김현민 4주차 과제 Step0 (#16)
    * Initial commit
    
    * Docs: Update README.md
    
    * Feat: Complete search screen layout
    
    * Feat: Add data to the database
    
    * Docs: Update README.md
    
    * Feat: Create item_view
    
    * Feat: Modify main view
    
    * Feat: Implement search list
    
    * Feat: Complete saved element view
    
    * Feat: Complete save action
    
    * Feat: Add persistent save function
    
    * 부산대 Android_김현민_2주차_과제_Step1 (#2)
    
    * Docs: Update README.md
    
    * Feat: Complete search screen layout
    
    * Feat: Add data to the database
    
    * Design: Modify save element design
    
    * Update README.md
    
    * Initial commit
    
    * Fix: Fix Week2 error
    
    * Docs: Update README.md
    
    Step1 README
    
    * Feat: Add initial settings
    
    * Feat: Add Document data class
    
    * Feat: Add RetrofitService interface
    
    * Refactor: Delete DB code
    
    * Feat: Add search function using API
    
    * Feat: Separate CategoryGroupCode
    
    * Docs: Update step2 README.md
    
    * Feat: complete initial project setup
    
    * Feat: Add mapview and map function
    
    * Fix: resolve errors
    
    * Chore: Update network security configuration
    
    * Fix: Resolve windows emulator issue
    
    * Style: Update mapview design
    
    * 부산대 Android_김현민 3주차 과제 Step0 (#19)
    
    * Initial commit
    
    * Docs: Update README.md
    
    * Feat: Complete search screen layout
    
    * Feat: Add data to the database
    
    * Docs: Update README.md
    
    * Feat: Create item_view
    
    * Feat: Modify main view
    
    * Feat: Implement search list
    
    * Feat: Complete saved element view
    
    * Feat: Complete save action
    
    * Feat: Add persistent save function
    
    * 부산대 Android_김현민_2주차_과제_Step1 (#2)
    
    * Docs: Update README.md
    
    * Feat: Complete search screen layout
    
    * Feat: Add data to the database
    
    * Design: Modify save element design
    
    * Update README.md
    
    ---------
    
    Co-authored-by: MyStoryG <bookwriterseo@gmail.com>
    
    * Fix: Reflect feedback
    
    * Fix: Resolve error
    
    ---------
    
    Co-authored-by: MyStoryG <bookwriterseo@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,11 @@
+
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
+
+fun getApiKey(key: String): String {
+    return gradleLocalProperties(rootDir, providers).getProperty(key) ?: ""
+}
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -14,7 +22,19 @@ android {
         versionCode = 1
         versionName = "1.0"
 
+
+        ndk {
+            abiFilters.add("arm64-v8a")
+            abiFilters.add("armeabi-v7a")
+            abiFilters.add("x86")
+            abiFilters.add("x86_64")
+        }
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField("String", "KAKAO_API_KEY", getApiKey("KAKAO_API_KEY"))
+        buildConfigField("String", "KAKAO_REST_API_KEY", getApiKey("KAKAO_REST_API_KEY"))
+        // manifest에서 사용하려고 만듦
+        manifestPlaceholders["KAKAO_API_KEY"] = getApiKey("KAKAO_API_KEY")
     }
 
     buildTypes {
@@ -36,21 +56,23 @@ android {
 
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.kakao.maps.open:android:2.9.5")
     implementation("androidx.activity:activity:1.8.0")
     implementation("androidx.test:core-ktx:1.5.0")
+    implementation("com.kakao.sdk:v2-all:2.20.3")
     testImplementation("junit:junit:4.13.2")
     testImplementation("io.mockk:mockk-android:1.13.11")
     testImplementation("io.mockk:mockk-agent:1.13.11")
--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -1,11 +1,198 @@
 package campus.tech.kakao.map
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.HorizontalScrollView
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import org.json.JSONArray
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: Adapter
+
+    lateinit var tvNoResult: TextView
+    lateinit var llSave: LinearLayoutCompat
+    lateinit var hScrollView: HorizontalScrollView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        키 해시 받기
+//        var keyHash = Utility.getKeyHash(this)
+//        Log.d("keyHash", keyHash)
+
+
+        val etSearch = findViewById<EditText>(R.id.etSearch)
+        tvNoResult = findViewById(R.id.tvNoResult)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val btnClose = findViewById<Button>(R.id.btnClose)
+        llSave = findViewById(R.id.llSave)
+        hScrollView = findViewById(R.id.hScrollView)
+
+        adapter = Adapter(mutableListOf())
+
+        recyclerView.adapter = adapter
+
+        tvNoResult.visibility = TextView.VISIBLE
+
+        etSearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                val search = s.toString()
+                if (search.isEmpty()) {
+                    showNoResults()
+                } else {
+                    CategoryGroupCode.categoryMap[search]?.let { categoryCode ->
+                        searchCategory(categoryCode)
+                    }
+                }
+            }
+        })
+
+        adapter.setOnItemClickListener(object : Adapter.OnItemClickListener {
+            override fun onItemClick(name: String) {
+                if (isProfileInSearchSave(name)) {
+                    removeSavedItem(name)
+                }
+                addSavedItem(name)
+            }
+        })
+
+        btnClose.setOnClickListener {
+            etSearch.text.clear()
+        }
+        loadSavedItems()
+    }
+
+    fun searchCategory(categoryGroupCode: String) {
+        Network.searchCategory(categoryGroupCode, object : Callback<KakaoResponse> {
+            override fun onResponse(call: Call<KakaoResponse>, response: Response<KakaoResponse>) {
+                if (response.isSuccessful) {
+                    searchProfiles(response.body())
+                } else {
+                    Toast.makeText(applicationContext, "응답 실패", Toast.LENGTH_SHORT).show()
+                    Log.e("MainActivity", "응답 실패")
+                }
+            }
+
+            override fun onFailure(call: Call<KakaoResponse>, t: Throwable) {
+                Toast.makeText(applicationContext, "요청 실패: ${t.message}", Toast.LENGTH_SHORT).show()
+                Log.e("MainActivity", "요청 실패", t)
+            }
+        })
+    }
+
+    fun searchProfiles(searchResult: KakaoResponse?) {
+        searchResult?.documents?.let { documents ->
+            if (documents.isEmpty()) {
+                showNoResults()
+            } else {
+                val profiles = documents.map { document ->
+                    Profile(document.name, document.address, document.type)
+                }
+                adapter.updateProfiles(profiles)
+                tvNoResult.visibility = View.GONE
+            }
+        } ?: showNoResults()
+    }
+
+    fun showNoResults() {
+        tvNoResult.visibility = View.VISIBLE
+        adapter.updateProfiles(emptyList())
+    }
+
+    override fun onPause() {
+        super.onPause()
+        saveSavedItems()
+    }
+
+    fun saveSavedItems() {
+        val sharedPreferences = getSharedPreferences("SavedItems", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val savedNames = JSONArray()
+        for (i in 0 until llSave.childCount) {
+            val savedView = llSave.getChildAt(i) as? ConstraintLayout
+            val tvSaveName = savedView?.findViewById<TextView>(R.id.tvSaveName)
+            if (tvSaveName != null) {
+                savedNames.put(tvSaveName.text.toString())
+            }
+        }
+        editor.putString("savedNames", savedNames.toString())
+        editor.apply()
+    }
+
+    fun loadSavedItems() {
+        val sharedPreferences = getSharedPreferences("SavedItems", MODE_PRIVATE)
+        val savedNamesString = sharedPreferences.getString("savedNames", "[]")
+        val savedNames = JSONArray(savedNamesString)
+        for (i in 0 until savedNames.length()) {
+            val name = savedNames.getString(i)
+            addSavedItem(name)
+        }
+    }
+
+
+    fun addSavedItem(name: String) {
+        val savedView = LayoutInflater.from(this)
+            .inflate(R.layout.search_save, llSave, false) as ConstraintLayout
+
+        val tvSaveName = savedView.findViewById<TextView>(R.id.tvSaveName)
+        val ivDelete = savedView.findViewById<ImageView>(R.id.ivDelete)
+
+        tvSaveName.text = name
+        ivDelete.setOnClickListener {
+            llSave.removeView(savedView)
+        }
+
+        llSave.addView(savedView)
+        hScrollView.visibility = View.VISIBLE
+        scrollToEndOfSearchSave()
+    }
+
+    fun removeSavedItem(name: String) {
+        for (i in 0 until llSave.childCount) {
+            val savedView = llSave.getChildAt(i) as? ConstraintLayout
+            val tvSaveName = savedView?.findViewById<TextView>(R.id.tvSaveName)
+            if (tvSaveName?.text.toString() == name) {
+                llSave.removeViewAt(i)
+                break
+            }
+        }
+    }
+
+    fun isProfileInSearchSave(name: String): Boolean {
+        for (i in 0 until llSave.childCount) {
+            val savedView = llSave.getChildAt(i) as? ConstraintLayout
+            val tvSaveName = savedView?.findViewById<TextView>(R.id.tvSaveName)
+            if (tvSaveName?.text.toString() == name) {
+                return true
+            }
+        }
+        return false
+    }
+
+    fun scrollToEndOfSearchSave() {
+        hScrollView.post {
+            hScrollView.fullScroll(View.FOCUS_RIGHT)
+        }
     }
 }
