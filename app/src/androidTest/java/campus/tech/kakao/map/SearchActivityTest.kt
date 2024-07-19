b2995fca0b9968123a89d8cd9ac3fc8d8849aaee
tlsgptj <angele0709@naver.com>
2024-07-19 16:27:37 +0900

    actionOnItemAtPosition함수 구현 중

--- app/src/androidTest/java/campus/tech/kakao/map/SearchActivityTest.kt
+++ app/src/androidTest/java/campus/tech/kakao/map/SearchActivityTest.kt
@@ -4,8 +4,11 @@ package campus.tech.kakao.map
 
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.runner.AndroidJUnit4
@@ -37,7 +40,7 @@ class SearchActivityTest {
     @Test
     fun testSearchView_textEntry() {
         onView(withId(R.id.search_text))
-            .perform(typeText("Cafe"), pressImeActionButton())
+            .perform(typeText("카페"), pressImeActionButton())
 
         onView(withId(R.id.RecyclerVer)).check(matches(hasDescendant(withText("Cafe Name"))))
     }
@@ -45,12 +48,14 @@ class SearchActivityTest {
     @Test
     fun testSearchResult_click() {
         onView(withId(R.id.search_text))
-            .perform(typeText("Library"), pressImeActionButton())
+            .perform(typeText("도서관"), pressImeActionButton())
 
         Thread.sleep(2000)
 
         onView(withId(R.id.RecyclerVer))
             .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
         intended(hasComponent(Map_Activity::class.java.name))
+        //recyclerver에 담긴 데이터를 클릭하면 그 데이터 위치로 이동하고 서칭을 해야하는데, actionOnItemAtPosition함수를 어떻게 구현해야할지 모르겠습니다.
     }
+
 }
