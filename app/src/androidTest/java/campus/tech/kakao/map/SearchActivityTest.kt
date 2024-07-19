763127a6840b25dba1310a52a486aeb42477c30a
tlsgptj <angele0709@naver.com>
2024-07-19 11:55:37 +0900

    SearchActivityTest.kt파일 작성

--- app/src/androidTest/java/campus/tech/kakao/map/SearchActivityTest.kt
+++ app/src/androidTest/java/campus/tech/kakao/map/SearchActivityTest.kt
@@ -1,4 +1,55 @@
 package campus.tech.kakao.map
 
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
 class SearchActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(Search_Activity::class.java)
+
+    @Test
+    fun testSearchView_isDisplayed() {
+        onView(withId(R.id.search_text)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testSearchRecyclerView_isDisplayed() {
+        onView(withId(R.id.RecyclerVer)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testSavedSearchRecyclerView_isDisplayed() {
+        onView(withId(R.id.recyclerHor)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testSearchView_textEntry() {
+        onView(withId(R.id.search_text))
+            .perform(typeText("Cafe"), pressImeActionButton())
+
+        onView(withId(R.id.RecyclerVer)).check(matches(hasDescendant(withText("Cafe Name"))))
+    }
+
+    @Test
+    fun testSearchResult_click() {
+        onView(withId(R.id.search_text))
+            .perform(typeText("Library"), pressImeActionButton())
+
+        Thread.sleep(2000)
+
+        onView(withId(R.id.RecyclerVer))
+            .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        intended(hasComponent(Map_Activity::class.java.name))
+    }
 }
