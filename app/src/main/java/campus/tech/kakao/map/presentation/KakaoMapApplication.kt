13d54b709ae50831601f534ef1beb37e7b6cb0d4
jooiss <jusong513@pusan.ac.kr>
2024-07-22 19:43:07 +0900

    chore: Rename application file name

--- app/src/main/java/campus/tech/kakao/map/presentation/KakaoMapApplication.kt
+++ app/src/main/java/campus/tech/kakao/map/presentation/KakaoMapApplication.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import campus.tech.kakao.map.BuildConfig
 import com.kakao.vectormap.KakaoMapSdk
 
-class KaKaoMapApplication : Application() {
+class KakaoMapApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         KakaoMapSdk.init(this, BuildConfig.KAKAO_API_KEY)
