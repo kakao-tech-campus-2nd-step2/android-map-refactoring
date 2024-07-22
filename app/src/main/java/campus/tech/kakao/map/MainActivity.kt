423ebd8635e0d303cdc0c06d5aa0378c389e4f72
KSC <ksc1008@naver.com>
2024-07-22 14:39:08 +0900

    fix: resolved import error

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -2,6 +2,7 @@ package campus.tech.kakao.map
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ksc.campus.tech.kakao.map.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
