179831796b05f502a99f535140b767e62d80c729
SHEEN <normengdie@pusan.ac.kr>
2024-07-22 21:14:38 +0900

    feat : Add Entity

---
+++ app/src/main/java/campus/tech/kakao/Model/SelectedData.kt
@@ -0,0 +1,12 @@
+package campus.tech.kakao.Model
+
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "selected_data")
+data class SelectedData(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0L,
+    @ColumnInfo(name = "name") val name: String
+)
