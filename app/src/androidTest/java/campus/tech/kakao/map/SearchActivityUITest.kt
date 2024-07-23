41995109b0cc1af7a5c9a58359e530db5662009f
jmc98 <jmc0820@naver.com>
2024-07-23 21:39:28 +0900

    Refactor[]: 추가 콜백 함수 onPlaceClicked

--- app/src/androidTest/java/campus/tech/kakao/map/SearchActivityUITest.kt
+++ app/src/androidTest/java/campus/tech/kakao/map/SearchActivityUITest.kt
@@ -6,7 +6,6 @@ import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withParent
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
