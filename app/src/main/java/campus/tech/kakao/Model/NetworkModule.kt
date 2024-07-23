af8f3e97a7eb7955c2daa1f07bc690ab3eb14bd0
SHEEN <normengdie@pusan.ac.kr>
2024-07-23 17:07:45 +0900

    feat : create NetworkModule

---
+++ app/src/main/java/campus/tech/kakao/Model/NetworkModule.kt
@@ -0,0 +1,31 @@
+package campus.tech.kakao.Model
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://dapi.kakao.com/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideKakaoApiService(retrofit: Retrofit): KakaoApiService {
+        return retrofit.create(KakaoApiService::class.java)
+    }
+}
