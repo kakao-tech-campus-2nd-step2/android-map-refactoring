0ef011af6f66f09eb56f7eba0a142f92f6c32a15
김정희 <165051638+lovelhee@users.noreply.github.com>
2024-07-24 03:09:10 +0900

    Feat: Hilt 초기화

--- app/src/main/java/campus/tech/kakao/map/application/AppClass.kt
+++ app/src/main/java/campus/tech/kakao/map/application/AppClass.kt
@@ -3,7 +3,9 @@ package campus.tech.kakao.map.application
 import android.app.Application
 import campus.tech.kakao.map.R
 import com.kakao.vectormap.KakaoMapSdk
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class AppClass : Application() {
     override fun onCreate() {
         super.onCreate()
