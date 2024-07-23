1952a0cca4ec68542fb8378e6118d7a4b5cef597
jooiss <jusong513@pusan.ac.kr>
2024-07-23 17:43:58 +0900

    feat: Add DAO interface

---
+++ app/src/main/java/campus/tech/kakao/map/data/PlaceDao.kt
@@ -0,0 +1,21 @@
+package campus.tech.kakao.map.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface PlaceDao {
+    @Insert
+    suspend fun insertPlace(place: Place)
+
+    @Delete
+    suspend fun deletePlace(place: Place)
+
+    @Query("DELETE FROM places WHERE name = :name")
+    suspend fun deletePlaceByName(name: String)
+
+    @Query("SELECT * FROM places")
+    suspend fun getAllPlaces() : MutableList<Place>
+}
