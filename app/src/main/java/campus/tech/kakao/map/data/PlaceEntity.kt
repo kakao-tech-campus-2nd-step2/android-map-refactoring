99d66d9ccb8eb8aa95523609b447f7a2b7541303
jooiss <jusong513@pusan.ac.kr>
2024-07-24 12:40:47 +0900

    feat: Add indices on 'name' and 'address' columns in PlaceEntity

--- app/src/main/java/campus/tech/kakao/map/data/PlaceEntity.kt
+++ app/src/main/java/campus/tech/kakao/map/data/PlaceEntity.kt
@@ -2,9 +2,10 @@ package campus.tech.kakao.map.data
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import campus.tech.kakao.map.domain.Place
 
-@Entity(tableName = "places", primaryKeys = ["name", "address"])
+@Entity(tableName = "places", primaryKeys = ["name", "address"], indices = arrayOf(Index(value = ["name", "address"], unique = true)))
 data class PlaceEntity(
     @ColumnInfo val name: String,
     @ColumnInfo val address: String,
