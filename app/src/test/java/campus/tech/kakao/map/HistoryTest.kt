0d012c16441d0a654995a36ed388bbf39fe429ac
sumin <cleonn7878@gmail.com>
2024-07-24 22:33:19 +0900

    refactor: Room 적용에 따른 유닛테스트 코드 수정

--- app/src/test/java/campus/tech/kakao/map/HistoryTest.kt
+++ app/src/test/java/campus/tech/kakao/map/HistoryTest.kt
@@ -1,41 +1,47 @@
 package campus.tech.kakao.map
 
 import android.content.Context
+import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import campus.tech.kakao.map.data.repository.HistoryRepositoryImpl
+import campus.tech.kakao.map.data.source.MapDatabase
 import campus.tech.kakao.map.data.source.MapDbHelper
+import campus.tech.kakao.map.domain.model.History
 import campus.tech.kakao.map.domain.model.Location
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import javax.inject.Inject
 
 @RunWith(RobolectricTestRunner::class)
 class HistoryTest {
-    lateinit var context: Context
-    lateinit var dbHelper: MapDbHelper
+    lateinit var mapDatabase: MapDatabase
     lateinit var historyRepositoryImpl: HistoryRepositoryImpl
 
     @Before
     fun init() {
-        context = ApplicationProvider.getApplicationContext()
-        dbHelper = MapDbHelper(context)
-        historyRepositoryImpl = HistoryRepositoryImpl(dbHelper)
+        mapDatabase = Room.databaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            MapDatabase::class.java,
+            "map"
+        ).build()
+        historyRepositoryImpl = HistoryRepositoryImpl(mapDatabase)
     }
 
     @Test
-    fun `검색 기록 저장 테스트`() {
-        val history = mutableListOf<Location>()
+    fun `검색 기록 저장 테스트`() = runTest {
+        val history = mutableListOf<History>()
         for (i in 1..10) {
-            val testLoc = Location(
+            val testHist = History(
                 "$i",
-                "testName$i",
-                "TEST",
-                "Test시 Test구 Test로 $i",
-                i + 11.0,i + 100.0)
-            history.add(testLoc)
-            historyRepositoryImpl.insertHistory(testLoc)
+                "testName$i"
+            )
+            history.add(testHist)
+            historyRepositoryImpl.insertHistory(testHist)
         }
         val savedHistory = historyRepositoryImpl.getAllHistory()
         history.forEachIndexed { index, location ->
@@ -44,15 +50,13 @@ class HistoryTest {
     }
 
     @Test
-    fun `ID가 일치하면 같은 Location으로 처리하여 중복으로 저장하지 않음`() {
+    fun `ID가 일치하면 같은 Location으로 처리하여 중복으로 저장하지 않음`() = runTest {
         for (i in 1..10) {
-            val testLoc = Location(
+            val testHist = History(
                 "0",
-                "$i",
-                "$i",
-                "${i}시 ${i}구 ${i}로",
-                1.0 + i,1.0 + i)
-            historyRepositoryImpl.insertHistory(testLoc)
+                "$i"
+            )
+            historyRepositoryImpl.insertHistory(testHist)
         }
         val savedHistory = historyRepositoryImpl.getAllHistory()
         assertEquals(savedHistory.size, 1)
--- app/src/test/java/campus/tech/kakao/map/LastLocationTest.kt
+++ app/src/test/java/campus/tech/kakao/map/LastLocationTest.kt
@@ -1,11 +1,14 @@
 package campus.tech.kakao.map
 
 import android.content.Context
+import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import campus.tech.kakao.map.data.repository.HistoryRepositoryImpl
 import campus.tech.kakao.map.data.repository.LastLocationRepositoryImpl
+import campus.tech.kakao.map.data.source.MapDatabase
 import campus.tech.kakao.map.data.source.MapDbHelper
 import campus.tech.kakao.map.domain.model.Location
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
 import org.junit.Before
@@ -15,25 +18,27 @@ import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
 class LastLocationTest {
-    lateinit var context: Context
-    lateinit var dbHelper: MapDbHelper
+    lateinit var mapDatabase: MapDatabase
     lateinit var lastLocationRepositoryImpl: LastLocationRepositoryImpl
 
     @Before
     fun init() {
-        context = ApplicationProvider.getApplicationContext()
-        dbHelper = MapDbHelper(context)
-        lastLocationRepositoryImpl = LastLocationRepositoryImpl(dbHelper)
+        mapDatabase = Room.databaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            MapDatabase::class.java,
+            "map"
+        ).build()
+        lastLocationRepositoryImpl = LastLocationRepositoryImpl(mapDatabase)
     }
 
     @Test
-    fun `마지막 위치가 존재하지 않는 경우 요청시 null을 반환한다`() {
+    fun `마지막 위치가 존재하지 않는 경우 요청시 null을 반환한다`() = runTest {
         val lastLocation = lastLocationRepositoryImpl.getLastLocation()
         assertNull(lastLocation)
     }
 
     @Test
-    fun `마지막 위치는 항상 이전 장소를 지우고 저장하여 1개만 가진다`() {
+    fun `마지막 위치는 항상 이전 장소를 지우고 저장하여 1개만 가진다`() = runTest {
         val history = mutableListOf<Location>()
         for (i in 1..10) {
             val testLoc = Location(
