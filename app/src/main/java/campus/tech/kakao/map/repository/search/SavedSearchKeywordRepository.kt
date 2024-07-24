03dc66e9247ccbec47fbe12a27fcb0a42309187e
sun <50648835+kimseongyu@users.noreply.github.com>
2024-07-25 04:26:20 +0900

    feat: change SQLite to Room about DB

--- app/src/main/java/campus/tech/kakao/map/repository/search/SavedSearchKeywordRepository.kt
+++ app/src/main/java/campus/tech/kakao/map/repository/search/SavedSearchKeywordRepository.kt
@@ -1,59 +1,30 @@
 package campus.tech.kakao.map.repository.search
 
-import android.content.ContentValues
 import android.content.Context
 import campus.tech.kakao.map.model.search.SearchKeyword
-import campus.tech.kakao.map.model.search.SearchKeywordEntry
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.withContext
 
 class SavedSearchKeywordRepository(context: Context) {
 
-    private val dbHelper = StoreInfoDBHelper(context)
+    private val db = SearchKeywordDB.getInstace(context)!!.searchKeywordDao()
 
-    fun saveSearchKeyword(searchKeyword: SearchKeyword) {
-        val db = dbHelper.writableDatabase
-        val values = ContentValues().apply {
-            put(SearchKeywordEntry.SEARCH_KEYWORD, searchKeyword.searchKeyword)
+    suspend fun saveSearchKeyword(searchKeyword: SearchKeyword) {
+        withContext(Dispatchers.IO) {
+            db.insert(searchKeyword)
         }
-        db.insert(SearchKeywordEntry.TABLE_NAME, null, values)
-        db.close()
     }
 
-    fun getSavedSearchKeywords(): List<SearchKeyword> {
-        val db = dbHelper.readableDatabase
-
-        val projection = arrayOf(
-            SearchKeywordEntry.SEARCH_KEYWORD
-        )
-
-        val cursor = db.query(
-            SearchKeywordEntry.TABLE_NAME,
-            projection,
-            null,
-            null,
-            null,
-            null,
-            null
-        )
-
-        val savedSearchKeywords = mutableListOf<SearchKeyword>()
-        with(cursor) {
-            while (moveToNext()) {
-                val searchWord = getString(getColumnIndexOrThrow(SearchKeywordEntry.SEARCH_KEYWORD))
-
-                val savedSearchWord = SearchKeyword(searchWord)
-                savedSearchKeywords.add(savedSearchWord)
-            }
+    suspend fun getSavedSearchKeywords(): List<SearchKeyword> {
+        return withContext(Dispatchers.IO) {
+            db.getAll()
         }
-        cursor.close()
-        db.close()
-        return savedSearchKeywords
     }
 
-    fun delSavedSearchKeyword(searchKeyword: SearchKeyword) {
-        val db = dbHelper.writableDatabase
-        val selection = "${SearchKeywordEntry.SEARCH_KEYWORD} = ?"
-        val selectionArgs = arrayOf(searchKeyword.searchKeyword)
-        db.delete(SearchKeywordEntry.TABLE_NAME, selection, selectionArgs)
-        db.close()
+    suspend fun delSavedSearchKeyword(searchKeyword: SearchKeyword) {
+        withContext(Dispatchers.IO) {
+            db.delete(searchKeyword)
+        }
     }
 }
