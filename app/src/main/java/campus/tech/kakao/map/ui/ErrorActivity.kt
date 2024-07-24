da573e2ecf98c836918d697d79552c1011371bb7
khyeonm <122lamb@naver.com>
2024-07-24 11:16:12 +0900

    Refactor: Apply MVVM in error activity
    Utilize LiveData and DataBinding

--- app/src/main/java/campus/tech/kakao/map/ui/ErrorActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/ui/ErrorActivity.kt
@@ -2,17 +2,23 @@ package campus.tech.kakao.map.ui
 
 import android.os.Bundle
 import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import campus.tech.kakao.map.R
+import campus.tech.kakao.map.databinding.ActivityErrorBinding
 
 class ErrorActivity : AppCompatActivity() {
+    val errorViewModel: ErrorViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_error)
+
+        val binding: ActivityErrorBinding = DataBindingUtil.setContentView(this, R.layout.activity_error)
+        binding.lifecycleOwner = this
+        binding.error = errorViewModel
 
         val errorMessage = intent.getStringExtra("errorMessage")
-        val tvError = findViewById<TextView>(R.id.tvError)
-        tvError.text = errorMessage
+        errorViewModel.setErrorMessage(errorMessage?:"Unknown")
 
     }
 }
