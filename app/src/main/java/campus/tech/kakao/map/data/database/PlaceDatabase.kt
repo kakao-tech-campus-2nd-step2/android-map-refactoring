692a002bdd797723081986b615dfbd1092532f74
Jeong hoon <hoondb@naver.com>
2024-07-25 19:49:41 +0900

    refactor: localRepository 가독성 개선

--- app/src/main/java/campus/tech/kakao/map/data/database/PlaceDatabase.kt
+++ app/src/main/java/campus/tech/kakao/map/data/database/PlaceDatabase.kt
@@ -6,6 +6,6 @@ import campus.tech.kakao.map.data.entity.PlaceEntity
 import campus.tech.kakao.map.data.entity.PlaceLogEntity
 
 @Database(entities = [PlaceEntity::class, PlaceLogEntity::class], version = 1)
-abstract class PlaceDatabase: RoomDatabase(){
+abstract class PlaceDatabase : RoomDatabase() {
     abstract fun placeDao(): PlaceDao
 }
