d8c67e11fdc46e8e66ffe01fd486944c3ddbc170
aengzu <jeon7879@naver.com>
2024-07-26 12:59:02 +0900

    refactor: add Qualifier for Database Name String injection
    - 데이터베이스 문자열 주입을 위해 @Qualifier 을 사용하였습니다
    - 아직 잘 모르는 개념이라 시간이 부족하여 주말동안 더 조사해보겠습니다.

--- app/src/main/java/campus/tech/kakao/map/di/DatabaseModule.kt
+++ app/src/main/java/campus/tech/kakao/map/di/DatabaseModule.kt
@@ -8,18 +8,28 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
 import javax.inject.Singleton
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DatabaseName
 
 @InstallIn(SingletonComponent::class)
 @Module
 object DatabaseModule {
+
+    @Provides
+    @DatabaseName
+    fun provideDatabaseName(): String = "kakao_map"
+
     @Provides
     @Singleton
-    fun provideDatabase(@ApplicationContext appContext: Context): AppDatabase {
+    fun provideDatabase(@ApplicationContext appContext: Context, @DatabaseName dbName: String): AppDatabase {
         return Room.databaseBuilder(
             appContext,
             AppDatabase::class.java,
-            "kakao_map"
+            dbName
         ).build()
     }
 
