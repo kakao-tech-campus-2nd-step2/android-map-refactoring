4554aa2eb8f5b97cfe2a08344f1e1d0e8e1ef804
kyleidea1 <99781357+kyleidea1@users.noreply.github.com>
2024-07-26 17:42:05 +0900

    feat: Injected dependencies into KyleMaps.kt

--- app/src/main/java/campus/tech/kakao/map/KyleMaps.kt
+++ app/src/main/java/campus/tech/kakao/map/KyleMaps.kt
@@ -3,15 +3,25 @@ package campus.tech.kakao.map
 import android.app.Application
 import campus.tech.kakao.map.di.AppModule
 import campus.tech.kakao.map.data.db.DataBase
+import campus.tech.kakao.map.repository.KakaoRepository
 import com.kakao.vectormap.KakaoMapSdk
+import dagger.hilt.android.HiltAndroidApp
+import retrofit2.Retrofit
 
+@HiltAndroidApp
 class KyleMaps : Application() {
 
     var isTestMode: Boolean = false
     lateinit var database: DataBase
+    lateinit var retrofit: Retrofit
+    lateinit var kakaoRepository: KakaoRepository
     override fun onCreate() {
         super.onCreate()
         KakaoMapSdk.init(this, getString(R.string.kakao_api_key))
         database = AppModule.provideDatabase(this)
+        retrofit = AppModule.provideRetrofit()
+        kakaoRepository = AppModule.provideKakaoRepository(retrofit)
+
+
     }
 }
