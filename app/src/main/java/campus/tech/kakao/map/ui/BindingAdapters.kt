c9b3deba31043d1164dca0282657224f28022c71
김정희 <165051638+lovelhee@users.noreply.github.com>
2024-07-26 22:56:10 +0900

    Remove: 커스텀 어댑터 삭제

--- app/src/main/java/campus/tech/kakao/map/ui/BindingAdapters.kt
+++ app/src/main/java/campus/tech/kakao/map/ui/BindingAdapters.kt
@@ -1,33 +1,33 @@
 package campus.tech.kakao.map.ui
-
-import android.widget.EditText
-import androidx.databinding.BindingAdapter
-import androidx.databinding.InverseBindingAdapter
-import androidx.databinding.InverseBindingListener
-import androidx.lifecycle.MutableLiveData
-
-@BindingAdapter("app:keyword")
-fun setKeyword(editText: EditText, keyword: MutableLiveData<String>?) {
-    if (keyword != null && editText.text.toString() != keyword.value) {
-        editText.setText(keyword.value)
-    }
-}
-
-@InverseBindingAdapter(attribute = "app:keyword")
-fun getKeyword(editText: EditText): String {
-    return editText.text.toString()
-}
-
-@BindingAdapter("app:keywordAttrChanged")
-fun setKeywordListener(editText: EditText, listener: InverseBindingListener?) {
-    if (listener != null) {
-        editText.addTextChangedListener(object : android.text.TextWatcher {
-            override fun afterTextChanged(s: android.text.Editable?) {
-                listener.onChange()
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-        })
-    }
-}
+//
+//import android.widget.EditText
+//import androidx.databinding.BindingAdapter
+//import androidx.databinding.InverseBindingAdapter
+//import androidx.databinding.InverseBindingListener
+//import androidx.lifecycle.MutableLiveData
+//
+//@BindingAdapter("app:keyword")
+//fun setKeyword(editText: EditText, keyword: MutableLiveData<String>?) {
+//    if (keyword != null && editText.text.toString() != keyword.value) {
+//        editText.setText(keyword.value)
+//    }
+//}
+//
+//@InverseBindingAdapter(attribute = "app:keyword")
+//fun getKeyword(editText: EditText): String {
+//    return editText.text.toString()
+//}
+//
+//@BindingAdapter("app:keywordAttrChanged")
+//fun setKeywordListener(editText: EditText, listener: InverseBindingListener?) {
+//    if (listener != null) {
+//        editText.addTextChangedListener(object : android.text.TextWatcher {
+//            override fun afterTextChanged(s: android.text.Editable?) {
+//                listener.onChange()
+//            }
+//
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+//        })
+//    }
+//}
