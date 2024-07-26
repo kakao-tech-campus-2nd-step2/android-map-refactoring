686de0863554b0935d8c2b081cdb88474434f5c9
tlsgptj <angele0709@naver.com>
2024-07-26 12:02:25 +0900

    4주차 수정

--- app/src/androidTest/java/campus/tech/kakao/map/SearchActivityTest.kt
+++ app/src/androidTest/java/campus/tech/kakao/map/SearchActivityTest.kt
@@ -2,16 +2,12 @@
 
 package campus.tech.kakao.map
 
-import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.runner.AndroidJUnit4
-import campus.tech.kakao.map.View.Map_Activity
 import campus.tech.kakao.map.View.Search_Activity
 import org.junit.Rule
 import org.junit.Test
