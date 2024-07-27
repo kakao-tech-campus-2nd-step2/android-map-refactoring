e55bf1c2c26d144f8cae0a8d110643ed4c4e916f
KSC <ksc1008@naver.com>
2024-07-27 16:52:29 +0900

    fix: removed a non-testable test

--- app/src/test/java/ksc/campus/tech/kakao/map/TestMapViewRepository.kt
+++ app/src/test/java/ksc/campus/tech/kakao/map/TestMapViewRepository.kt
@@ -2,14 +2,11 @@ package ksc.campus.tech.kakao.map
 
 import android.content.Context
 import android.content.SharedPreferences
-import com.kakao.vectormap.camera.CameraPosition
 import io.mockk.every
 import io.mockk.just
 import io.mockk.mockkClass
 import io.mockk.runs
-import io.mockk.spyk
 import io.mockk.verify
-import kotlinx.coroutines.runBlocking
 import ksc.campus.tech.kakao.map.data.datasources.MapPreferenceLocalDataSource
 import ksc.campus.tech.kakao.map.data.repositoryimpls.MapViewRepositoryImpl
 import org.junit.Before
@@ -55,37 +52,13 @@ class TestMapViewRepository {
 
         // when
         val repository = MapViewRepositoryImpl(
-            MapPreferenceLocalDataSource(),
+            dataSource,
             mockContext
         )
 
         // then
         verify {
             mockSharedPreferences.registerOnSharedPreferenceChangeListener(any())
-        }
-    }
-
-    @Test
-    fun `데이터를 업데이트 하면 자동으로 갱신된 데이터를 불러온다`(){
-        // given
-        val dataSource = MapPreferenceLocalDataSource()
-        val someCameraPosition = CameraPosition.from(0.0, 0.0, 0, 0.0, 0.0, 0.0)
-
-        val repository = spyk(MapViewRepositoryImpl(
-            MapPreferenceLocalDataSource(),
-            mockContext
-        ))
-
-        // when
-        runBlocking {
-            repository.updateCameraPosition(someCameraPosition)
-        }
-
-        // then
-        verify {
-            runBlocking {
-                repository["loadSavedCurrentPosition"]()
-            }
         }
     }
 }
