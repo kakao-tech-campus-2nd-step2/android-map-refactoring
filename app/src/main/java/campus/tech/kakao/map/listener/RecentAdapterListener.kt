dacda2ca529c1ae4296cf8e439696b4693fe1e2f
yb0x00 <yeongbeen0x00@gmail.com>
2024-07-28 03:26:32 +0900

    remove: Remove unused files

--- app/src/main/java/campus/tech/kakao/map/listener/RecentAdapterListener.kt
+++
@@ -1,5 +0,0 @@
-package campus.tech.kakao.map.listener
-
-interface RecentAdapterListener {
-    fun autoSearch(searchData: String)
-}
--- app/src/main/java/campus/tech/kakao/map/listener/SearchAdapterListener.kt
+++
@@ -1,5 +0,0 @@
-package campus.tech.kakao.map.listener
-
-interface SearchAdapterListener {
-    fun displaySearchLocation(name: String, address: String, latitude: String, longitude: String)
-}
