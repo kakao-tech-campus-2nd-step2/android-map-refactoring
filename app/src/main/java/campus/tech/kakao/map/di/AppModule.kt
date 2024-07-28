f207fec88239e0e534d8be7b038f84a90d5408a1
Dong-Han <donghk77@naver.com>
2024-07-29 01:33:39 +0900

    fix : 수정

--- app/src/main/java/campus/tech/kakao/map/di/AppModule.kt
+++ app/src/main/java/campus/tech/kakao/map/di/AppModule.kt
@@ -20,16 +20,19 @@ object AppModule {
     @Singleton
     @Provides
     fun provideRetrofitService(): RetrofitService {
+        // RetrofitInstance.api 호출 -> RetrofitService 생성
         return RetrofitInstance.api
     }
 
     @Singleton
     @Provides
+    // 기본 공유 환경 설정 생성
     fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
         return PreferenceManager.getDefaultSharedPreferences(context)
     }
 
     @Provides
+    // retrofitService, sharedPreferences 인자 -> PlaceViewModel 생성
     fun providePlaceViewModel(
         retrofitService: RetrofitService,
         sharedPreferences: SharedPreferences
