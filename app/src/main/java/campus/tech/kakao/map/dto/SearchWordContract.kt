8ddb5b8b68e439a5f1f4b79e705ec632a89ed5a2
jmc98 <jmc0820@naver.com>
2024-07-28 19:29:01 +0900

    Refactor[SearchWordContract]: 삭제 사용하지 않는 상수

--- app/src/main/java/campus/tech/kakao/map/dto/SearchWordContract.kt
+++ app/src/main/java/campus/tech/kakao/map/dto/SearchWordContract.kt
@@ -4,8 +4,5 @@ object SearchWordContract {
 	const val DB_NAME = "search_word"
 	const val DB_VERSION = 1
 	const val TABLE_NAME = "SearchWord"
-	const val COLUMN_NAME_NAME = "name"
-	const val COLUMN_NAME_ADDRESS = "address"
-	const val COLUMN_NAME_TYPE = "type"
 
 }
