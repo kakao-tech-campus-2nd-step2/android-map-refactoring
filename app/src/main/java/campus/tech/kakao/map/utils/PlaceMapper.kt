871dfe2f48b61e0afca80010c02890fcebd81bde
aengzu <jeon7879@naver.com>
2024-07-29 14:08:58 +0900

    refactor: Mapper 관련 피드백 반영

--- app/src/main/java/campus/tech/kakao/map/utils/PlaceMapper.kt
+++ app/src/main/java/campus/tech/kakao/map/utils/PlaceMapper.kt
@@ -2,26 +2,16 @@ package campus.tech.kakao.map.utils
 
 import campus.tech.kakao.map.data.local.entity.VisitedPlaceEntity
 import campus.tech.kakao.map.domain.dto.PlaceVO
+import campus.tech.kakao.map.domain.dto.toPlaceVO
+import campus.tech.kakao.map.domain.dto.toVisitedPlaceEntity
 
 class PlaceMapper {
     fun mapToEntity(place: PlaceVO) : VisitedPlaceEntity {
-        return VisitedPlaceEntity(
-            placeName = place.placeName,
-            addressName = place.addressName,
-            categoryName = place.categoryName,
-            latitude = place.latitude,
-            longitude = place.longitude
-        )
+        return place.toVisitedPlaceEntity()
     }
 
     fun mapFromEntity(entity: VisitedPlaceEntity): PlaceVO {
-        return PlaceVO(
-            placeName = entity.placeName,
-            addressName = entity.addressName,
-            categoryName = entity.categoryName,
-            latitude = entity.latitude,
-            longitude = entity.longitude
-        )
+        return entity.toPlaceVO()
     }
 
 }
