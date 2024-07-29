360d5dd9ebbd98659a028c885db5f6f8a71ef4bd
arieum <lar713@naver.com>
2024-07-29 14:42:02 +0900

    Refactor: Apply @ApplicationContext

--- app/src/main/java/campus/tech/kakao/map/data/di/RepositoryModule.kt
+++ app/src/main/java/campus/tech/kakao/map/data/di/RepositoryModule.kt
@@ -15,6 +15,7 @@ import campus.tech.kakao.map.repository.PlaceRepositoryInterface
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -24,8 +25,8 @@ object RepositoryModule {
 
     @Provides
     @Singleton
-    fun provideContext(application: Application): Context {
-        return application.applicationContext
+    fun provideContext(@ApplicationContext context: Context): Context {
+        return context
     }
 
 
