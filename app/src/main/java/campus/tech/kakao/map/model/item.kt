12cb5236234ed176a865673792c78593425816b0
nJiyeon <150491973+nJiyeon@users.noreply.github.com>
2024-07-29 14:40:53 +0900

    Delete app/src/main/java/campus/tech/kakao/map/model/item.kt

--- app/src/main/java/campus/tech/kakao/map/model/item.kt
+++
@@ -1,9 +0,0 @@
-package campus.tech.kakao.map.model
-
-data class Item(
-    val place: String,
-    val address: String,
-    val category: String,
-    val latitude: Double,
-    val longitude:Double
-)
